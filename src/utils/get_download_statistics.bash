f48f819e5e9dab7c46a24974fac83be81eb068eb
Kai Ellinger <coding@blicke.de>
2024-05-02 22:08:32 +0200

    testing new naming schema for versioned downloads; add script to view download statistics

---
+++ src/utils/get_download_statistics.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# gh auth login
+
+gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" \
+/repos/freeella/kubectl-cpbase64/releases | \
+jq 'sort_by(.tag_name) | .[] | {  "tag_name": ."tag_name", "assets": .assets[] | {"name": .name, "download_count": ."download_count", "created_at": ."created_at" } }  ' -c 
+
