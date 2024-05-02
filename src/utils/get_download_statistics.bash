eb2d92964a6885917c159ef0c99cf5d3b667f1f3
Kai Ellinger <coding@blicke.de>
2024-05-02 23:01:00 +0200

    add GH documentation url to get_download_statistics.bash

--- src/utils/get_download_statistics.bash
+++ src/utils/get_download_statistics.bash
@@ -2,6 +2,8 @@
 
 # gh auth login
 
+# See: https://docs.github.com/de/rest/releases/releases?apiVersion=2022-11-28#list-assets-for-a-release
+
 gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" \
 /repos/freeella/kubectl-cpbase64/releases | \
 jq 'sort_by(.tag_name) | .[] | {  "tag_name": ."tag_name", "assets": .assets[] | {"name": .name, "download_count": ."download_count", "created_at": ."created_at" } }  ' -c
